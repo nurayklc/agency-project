4361f48552e66cc6913eca74b67f7488e64489c7
nurayklc <nuraykilic0101@gmail.com>
2022-03-16 23:54:32 +0300

    Create portfolio and connect db

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const ejs = require('ejs');
+const portfolioController = require('./controllers/portfolioController');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
+// CONNECT DB dbUser 5gu8994Htd4N97U
+mongoose.connect('mongodb+srv://dbUser:5gu8994Htd4N97U@cluster0.veenn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+
 // MIDDLEWARES
 app.use(express.static('public'));
-/* app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
- */
+
+
+// ROUTING
 app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/add', function (req, res) {
-  res.render('add');
-});
+app.post('/portfolio', portfolioController.addPortfolio);
 
 const port = 3000;
 app.listen(port, () => {
